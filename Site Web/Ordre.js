55edc7eda9c00c6654dc54f933ef068221f4bc24
Lussiez Julien <70692644+JLussiez@users.noreply.github.com>
2022-01-03 14:05:13 +0100

    Add files via upload

---
+++ Site Web/Ordre.js
@@ -0,0 +1,28 @@
+//Envoyer commande en Héxa au C++
+//Envoier commande en héxa
+
+function Ordre(Ordre)
+{
+    //Envoier commande en héxa
+    //var socket = new WebSocket("ws://192.168.65.28:16050");
+    socket.send(Ordre);
+}
+
+function Connect(lien)
+{
+    //Envoier commande en héxa
+    var socket = new WebSocket(lien);
+}
+
+function CacherEdit()
+{
+    let Creation = document.getElementById("NewCam");
+    let Edit = document.getElementById("EditCam");
+    let Bouton = document.getElementById("Boutton");
+    let Suppr = document.getElementById("Suppr")
+
+    Creation.style.display = "block"
+    Edit.style.display = "none";
+    Bouton.style.display = "none";
+    Suppr.style.display = "none";
+}
