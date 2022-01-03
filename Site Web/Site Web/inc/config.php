db6f202e2de50845639c0053007ceb434784e846
Lussiez Julien <70692644+JLussiez@users.noreply.github.com>
2022-01-03 14:03:52 +0100

    Add files via upload

---
+++ Site Web/Site Web/inc/config.php
@@ -0,0 +1,18 @@
+<?php 
+session_start();
+
+// Include DB CONFIG 
+require_once "db.php";
+
+// TABLE OF CONFIG
+$_CONFIG = array();
+
+// Init class
+require "class/user.class.php";
+$user = new User($_SQL);
+
+require "class/cam.class.php";
+$cam = new Cam($_SQL);
+
+require "class/move.class.php";
+$camMove = new CamMove();
